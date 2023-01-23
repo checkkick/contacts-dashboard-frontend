2031bb3bc2309664324beef33a5b810e7df26d20
Konstantin <tuskancik226@gmail.com>
2023-01-23 13:01:21 +0300

    prettier config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
   env: {
     browser: true,
   },
   rules: {
-    'no-alert': 0,
     'no-param-reassign': [2, { props: false }],
-    'no-plusplus': 0,
-    'no-iterator': 0,
     'no-restricted-syntax': [2, 'WithStatement'],
-    'func-style': 0,
-    'operator-linebreak': 0,
-    'comma-dangle': 0,
-    'object-curly-newline': 0,
   },
 };
